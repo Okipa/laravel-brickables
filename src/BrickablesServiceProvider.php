f8ca38bd55cd037b01e056ec9199bba599d610e9
Arthur LORENT <arthur@acid.fr>
2020-01-10 15:49:27 +0100

    wip

--- src/BrickablesServiceProvider.php
+++ src/BrickablesServiceProvider.php
@@ -2,14 +2,18 @@
 
 namespace Okipa\LaravelBrickables;
 
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class BrickablesServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
+     *
+     * @param \Illuminate\Filesystem\Filesystem $filesystem
      */
-    public function boot()
+    public function boot(Filesystem $filesystem)
     {
         $this->loadViewsFrom(__DIR__ . '/../ressources/views', 'laravel-brickables');
         $this->loadTranslationsFrom(__DIR__ . '/../ressources/lang', 'laravel-brickables');
@@ -22,13 +26,29 @@ class BrickablesServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../ressources/lang' => resource_path('lang/vendor/laravel-brickables'),
         ]);
-        if (! class_exists('CreateBricksTable')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_bricks_table.php.stub' => database_path(
-                    'migrations/' . date('Y_m_d_His', time()) . '_create_bricks_table.php'
-                ),
-            ], 'laravel-brickables:migrations');
-        }
+        $this->publishes([
+            __DIR__
+            . '/../database/migrations/create_bricks_table.php.stub' => $this->getMigrationFileName($filesystem),
+        ], 'laravel-brickables:migrations');
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param Filesystem $filesystem
+     *
+     * @return string
+     */
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath() . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path . '*_create_bricks_tables.php');
+            })
+            ->push($this->app->databasePath("/migrations/{$timestamp}_create_bricks_tables.php"))
+            ->first();
     }
 
     /**
