aab99ce48c0815892e56f8da646445ad9177f45e
Artu1000 <arthur.lorent@gmail.com>
2020-01-12 15:56:30 +0100

    wip

--- src/Controllers/BricksController.php
+++ src/Controllers/BricksController.php
@@ -3,14 +3,21 @@
 namespace Okipa\LaravelBrickables\Controllers;
 
 use Illuminate\Http\Request;
+use Okipa\LaravelBrickables\Abstracts\Brickable;
 
 class BricksController
 {
     /**
      * @param Request $request
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function create(Request $request)
     {
-        dd($request->all());
+        $brick = null;
+        /** @var Brickable $brickable */
+        $brickable = app($request->brickable);
+
+        return view($brickable->getFormViewPath(), compact('brick', 'brickable'));
     }
 }
