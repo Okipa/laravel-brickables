13e87f90d283cf6368671356a21d4fa6f82f892c
Artu1000 <arthur.lorent@gmail.com>
2020-01-13 15:43:51 +0100

    wip

--- ressources/views/admin/partials/panel-edit.blade.php
+++ ressources/views/admin/partials/panel-edit.blade.php
@@ -1,4 +1,5 @@
 <form class="mx-1" role="form" method="GET" action="{{ $brick->brickable->getEditRoute($brick) }}">
+    <input type="hidden" name="admin_panel_url" value="{{ url()->current() }}">
     <button class="btn btn-link p-0" type="submit" title="@lang('Edit')">
         <i class="fas fa-edit fa-fw"></i>
     </button>
