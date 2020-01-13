36237668d611b78883a909a926bf0b2882b45099
Artu1000 <arthur.lorent@gmail.com>
2020-01-13 17:11:34 +0100

    wip

--- ressources/views/admin/partials/panel-create.blade.php
+++ ressources/views/admin/partials/panel-create.blade.php
@@ -1,13 +1,16 @@
-<form role="form" method="GET" action="{{ route('brick.create') }}">
+<form class="form-inline" role="form" method="GET" action="{{ route('brick.create') }}">
     <input type="hidden" name="model_id" value="{{ $model->id }}">
     <input type="hidden" name="model_type" value="{{ get_class($model) }}">
     <input type="hidden" name="admin_panel_url" value="{{ url()->current() }}#bricks-admin-panel">
-    <select name="brickable_type">
-        <option value="">@lang('Select a brickable')</option>
+    <select class="custom-select mr-3" name="brickable_type">
+        <option value="">@lang('validation.attributes.brickable_type')</option>
         @foreach(Brickables::getAll() as $brickable)
             <option value="{{ get_class($brickable) }}">{{ $brickable->getLabel() }}</option>
         @endforeach
     </select>
+    @error('brickable_type')
+        <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
     <button class="btn btn-primary" type="submit" title="@lang('Create')">
         <i class="fas fa-plus-circle fa-fw"></i> @lang('Add')
     </button>
--- ressources/views/admin/partials/validation-errors.blade.php
+++ ressources/views/admin/partials/validation-errors.blade.php
@@ -1,6 +1,6 @@
 @if ($errors->any())
     <div class="alert alert-danger">
-        <ul>
+        <ul class="m-0">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
