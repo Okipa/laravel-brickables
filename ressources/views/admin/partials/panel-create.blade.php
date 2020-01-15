7f25cbad794372f91a4560c07e750b47171b110c
Arthur LORENT <arthur@acid.fr>
2020-01-15 15:33:48 +0100

    wip

--- ressources/views/admin/partials/panel-create.blade.php
+++ ressources/views/admin/partials/panel-create.blade.php
@@ -2,7 +2,7 @@
     <input type="hidden" name="model_id" value="{{ $model->id }}">
     <input type="hidden" name="model_type" value="{{ get_class($model) }}">
     <input type="hidden" name="admin_panel_url" value="{{ url()->current() }}#bricks-admin-panel">
-    <div>
+    <div class="mr-3">
         <div class="input-group">
             <div class="input-group-prepend">
                 <span class="input-group-text">
