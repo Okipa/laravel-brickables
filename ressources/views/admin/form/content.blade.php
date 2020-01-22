f746054375fc59a3755b98ad67271bc8e63e7062
Arthur LORENT <arthur@acid.fr>
2020-01-22 10:33:55 +0100

    wip

--- ressources/views/admin/form/content.blade.php
+++ ressources/views/admin/form/content.blade.php
@@ -1,12 +1,17 @@
-<form method="POST"
-      action="{{ $brick ? $brickable->getUpdateRoute($brick) : $brickable->getStoreRoute() }}"
-      enctype="multipart/form-data">
-    @csrf
-    @if($brick)@method('PUT')@endif
-    <input type="hidden" name="model_id" value="{{ $model->id }}">
-    <input type="hidden" name="model_type" value="{{ get_class($model) }}">
-    <input type="hidden" name="brickable_type" value="{{ get_class($brickable) }}">
-    <input type="hidden" name="admin_panel_url" value="{{ $adminPanelUrl }}">
-    @yield('inputs')
-    @include('laravel-brickables::admin.form.actions')
-</form>
+<div class="card">
+    <div class="card-body">
+        <form method="POST"
+              action="{{ $brick ? $brickable->getUpdateRoute($brick) : $brickable->getStoreRoute() }}"
+              enctype="multipart/form-data">
+            @csrf
+            @if($brick)@method('PUT')@endif
+            <input type="hidden" name="model_id" value="{{ $model->id }}">
+            <input type="hidden" name="model_type" value="{{ get_class($model) }}">
+            <input type="hidden" name="brickable_type" value="{{ get_class($brickable) }}">
+            <input type="hidden" name="admin_panel_url" value="{{ $adminPanelUrl }}">
+            @yield('inputs')
+            @include('laravel-brickables::admin.form.actions')
+        </form>
+    </div>
+</div>
+
