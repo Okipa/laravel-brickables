c61d11d046abb11390361b664dc3c28db0fcd84a
Arthur LORENT <arthur@acid.fr>
2020-01-23 15:31:15 +0100

    wip

--- ressources/views/admin/panel/create.blade.php
+++ ressources/views/admin/panel/create.blade.php
@@ -2,25 +2,23 @@
     <input type="hidden" name="model_id" value="{{ $model->id }}">
     <input type="hidden" name="model_type" value="{{ get_class($model) }}">
     <input type="hidden" name="admin_panel_url" value="{{ url()->current() }}#bricks-admin-panel">
-    <div class="mr-3">
-        <div class="form-group m-0">
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <i class="fas fa-shapes"></i>
-                    </span>
-                </div>
-                <select class="custom-select{{ optional($errors ?? null)->has('brickable_type') ? ' is-invalid' : null }}"
-                        name="brickable_type">
-                    <option value="">@lang('validation.attributes.brickable_type')</option>
-                    @foreach(Brickables::getAll() as $brickable)
-                        <option value="{{ get_class($brickable) }}">{{ $brickable->getLabel() }}</option>
-                    @endforeach
-                </select>
-                @if(optional($errors ?? null)->has('brickable_type'))
-                    <div class="invalid-feedback">{{ $errors->first('brickable_type') }}</div>
-                @endif
+    <div class="form-group mb-0 mr-3">
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text">
+                    <i class="fas fa-shapes"></i>
+                </span>
             </div>
+            <select class="custom-select{{ optional($errors ?? null)->has('brickable_type') ? ' is-invalid' : null }}"
+                    name="brickable_type">
+                <option value="">@lang('validation.attributes.brickable_type')</option>
+                @foreach(Brickables::getAll() as $brickable)
+                    <option value="{{ get_class($brickable) }}">{{ $brickable->getLabel() }}</option>
+                @endforeach
+            </select>
+            @if(optional($errors ?? null)->has('brickable_type'))
+                <div class="invalid-feedback">{{ $errors->first('brickable_type') }}</div>
+            @endif
         </div>
     </div>
     <button class="btn btn-primary" type="submit" title="@lang('Create')">
