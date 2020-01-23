9f1e764b28c6763e784c6553e3beb46bad290e52
Arthur LORENT <arthur@acid.fr>
2020-01-23 16:43:16 +0100

    wip

--- ressources/views/brickables/one-text-column/brick.blade.php
+++ ressources/views/brickables/one-text-column/brick.blade.php
@@ -1,5 +1,7 @@
-<div class="row">
-    <div class="col-12">
-        {{ data_get($brick, 'data.text') }}
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            {{ data_get($brick, 'data.text') }}
+        </div>
     </div>
 </div>
