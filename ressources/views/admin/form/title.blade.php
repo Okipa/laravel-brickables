57219bd4370130337162bdaa88a3f7090b459be8
Arthur LORENT <arthur@acid.fr>
2020-01-27 16:10:56 +0100

    wip

--- ressources/views/admin/form/title.blade.php
+++ ressources/views/admin/form/title.blade.php
@@ -1,2 +1,5 @@
-<h1>{{ $model->getReadableClassName() }} > @lang('Content bricks') > {{ $brickable->getLabel() }} > @lang($brick ? 'Edition' : 'Creation')</h1>
+<h1>
+    <i class="fas fa-th-large fa-fw"></i>
+    {{ $model->getReadableClassName() }} > @lang('Content bricks') > {{ $brickable->getLabel() }} > @lang($brick ? 'Edition' : 'Creation')
+</h1>
 <hr>
