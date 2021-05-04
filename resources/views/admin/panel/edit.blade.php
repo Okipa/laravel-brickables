7e1e40e25de1ba54f886b33c17289a4ba1e1bcf1
Quentin <balsakup@gmail.com>
2021-05-04 20:59:51 +0200

    Change current url with full url for admin_panel_url (#13)

--- resources/views/admin/panel/edit.blade.php
+++ resources/views/admin/panel/edit.blade.php
@@ -1,5 +1,5 @@
 <form class="mx-1" role="form" method="GET" action="{{ $brick->brickable->getEditRoute($brick) }}">
-    <input type="hidden" name="admin_panel_url" value="{{ url()->current() }}#bricks-admin-panel">
+    <input type="hidden" name="admin_panel_url" value="{{ url()->full() }}#bricks-admin-panel">
     <button class="btn btn-link p-0" type="submit" title="@lang('Edit')">
         <i class="fas fa-edit fa-fw"></i>
     </button>
